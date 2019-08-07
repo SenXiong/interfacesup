099a286bd3bd991a44b0676c21a629d3fa6f6b4b
SenXiong <love-8x8j@qq.com>
2019-08-07 16:59:40 +0800

    方法Api方法添加

--- src/main/java/com/xj/interfacesup/controller/testController.java
+++ src/main/java/com/xj/interfacesup/controller/testController.java
@@ -1,14 +1,20 @@
 package com.xj.interfacesup.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(value = "测试Api")
 @RestController
 @RequestMapping(value="/test")
 public class testController {
+
+    @ApiOperation(value = "get方法，返回常量",notes = "测试方法")
     @RequestMapping(value="/say",method = RequestMethod.GET)
     public String test(){
         return "hello,didi!";
     }
+
 }
